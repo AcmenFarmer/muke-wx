78eb1d708ec128d1a37c12bad98b1f9b12007365
Jiaqi <wangjq@polysoft.com.cn>
2019-03-14 16:22:32 +0800

    实现音乐组件播放功能

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,17 +1,21 @@
 // components/classic/music/index.js
-import { classicBeh } from '../classic-beh'
+import { classicBeh } from '../classic-beh';
+const mMgr = wx.getBackgroundAudioManager();
 Component({
   behaviors: [classicBeh],
   /**
    * 组件的属性列表
    */
   properties: {
+    src: String,
+    title: String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
+    palying: false,
     pauseSrc: 'images/player@playing.png',
     playSrc: 'images/player@waitting.png'
   },
@@ -20,6 +24,12 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onPlay: function () {
+      this.setData({
+        palying: true
+      })
+      mMgr.title = this.properties.title;
+      mMgr.src = this.properties.src;
+    }
   }
 })
