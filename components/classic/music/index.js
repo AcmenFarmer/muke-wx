e2f5ead0c4817e104d53c7c58e6780f04329520a
Jiaqi <wangjq@polysoft.com.cn>
2019-03-15 11:49:04 +0800

    音乐组件完成

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,5 +1,7 @@
 // components/classic/music/index.js
-import { classicBeh } from '../classic-beh';
+import {
+  classicBeh
+} from '../classic-beh';
 const mMgr = wx.getBackgroundAudioManager();
 Component({
   behaviors: [classicBeh],
@@ -20,16 +22,58 @@ Component({
     playSrc: 'images/player@waitting.png'
   },
 
+  attached: function(){
+    this._recoverStatus();
+    this._monitorSwitch();
+  },
+
   /**
    * 组件的方法列表
    */
   methods: {
     onPlay: function () {
-      this.setData({
-        palying: true
+      if (!this.data.palying) {
+        this.setData({
+          palying: true
+        })
+        mMgr.title = this.properties.title;
+        mMgr.src = this.properties.src;
+      } else {
+        this.setData({
+          palying: false
+        })
+        mMgr.pause();
+      }
+    },
+    _recoverStatus: function() {
+      // 如果当前是暂停状态，设置图片为暂停状态
+      if(mMgr.paused){
+        this.setData({
+          palying: false
+        })
+        return;
+      }
+
+      // src相同说明当前音乐为播放状态
+      if(mMgr.src == this.properties.src){
+        this.setData({
+          palying: true
+        })
+      }
+    },
+    _monitorSwitch: function() {
+      mMgr.onPlay(() => {
+        this._recoverStatus();
       })
-      mMgr.title = this.properties.title;
-      mMgr.src = this.properties.src;
+      mMgr.onPause(() => {
+        this._recoverStatus();
+      })
+      mMgr.onStop(() => {
+        this._recoverStatus();
+      })
+      mMgr.onEnded(() => {
+        this._recoverStatus();
+      })
     }
   }
 })
